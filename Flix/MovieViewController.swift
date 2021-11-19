9bf2c2bf296ecb90f937fa39378ff4ff7674be2e
Kabir Dhillon <kabirdhillon99@gmail.com>
2021-11-19 14:16:07 -0800

    Initial commit

---
+++ Flix/MovieViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MovieViewController.swift
+//  Flix
+//
+//  Created by Kabir Dhillon on 11/17/21.
+//
+
+import SwiftUI
+
+struct MovieViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieViewController()
+    }
+}
