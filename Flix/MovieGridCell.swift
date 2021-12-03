24e63accde67bf97024df08879f37dcb1eaff809
Kabir Dhillon <kabirdhillon99@gmail.com>
2021-12-03 00:42:52 -0800

    Flix Part 2
    Part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Kabir Dhillon on 12/2/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIView!
+}
