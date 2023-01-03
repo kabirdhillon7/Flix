f9a1be0afee41af849f99454637528ab137853fb
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-01-02 17:48:54 -0800

    Updated targets for iOS 11

---
+++ Flix/Resources/APICaller.swift
@@ -0,0 +1,17 @@
+//
+//  APICaller.swift
+//  Flix
+//
+//  Created by Kabir Dhillon on 1/2/23.
+//
+
+import Foundation
+
+class APICaller {
+    
+    let apiKey: String = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+    
+    // Fn to get movies
+    
+    // Fn to get trailer
+}
