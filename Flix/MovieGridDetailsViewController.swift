b22257504c77f72973a0172668647c57d39f2055
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-01-02 22:57:47 -0800

    Update MovieGridDetailsViewController.swift

--- Flix/MovieGridDetailsViewController.swift
+++ Flix/MovieGridDetailsViewController.swift
@@ -10,7 +10,7 @@ import AlamofireImage
 import Cosmos
 
 class MovieGridDetailsViewController: UIViewController {
-
+    
     @IBOutlet weak var backdropView: UIImageView!
     @IBOutlet weak var posterView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
@@ -21,7 +21,7 @@ class MovieGridDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         titleLabel.text = superheroMovie.title
         titleLabel.sizeToFit()
@@ -45,5 +45,5 @@ class MovieGridDetailsViewController: UIViewController {
         ratingView.settings.emptyImage = UIImage(named: "star")
         ratingView.settings.textColor = UIColor.white
     }
-
+    
 }
