b689082e0eb195d0f01e17c943a78857dc8d0cac
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-01-02 22:57:43 -0800

    MoviesDetailVC
    Added a func to handle ratingBar settings/configurations

--- Flix/MovieDetailsViewController.swift
+++ Flix/MovieDetailsViewController.swift
@@ -37,17 +37,21 @@ class MovieDetailsViewController: UIViewController {
         backdropView.af.setImage(withURL: movie.backdropUrl)
         
         ratingView.text = "\(String(describing: movie.rating))"
+        setRatingNumberSettings()
+        
+        /*
+         let movieID = movie["id"]
+         let videoes = "https://api.themoviedb.org/3/movie/\(movieID)/videos?api_key=93e318361fd7d5007c54fbedb1dc26ee&language=en-US"
+         
+         playerView.load(withVideoId: "")*/
+        
+    }
+    
+    func setRatingNumberSettings() {
         ratingView.settings.filledColor = UIColor.yellow
         ratingView.settings.emptyBorderColor = UIColor.yellow
         ratingView.settings.filledImage = UIImage(named: "star.fill")
         ratingView.settings.emptyImage = UIImage(named: "star")
         ratingView.settings.textColor = UIColor.white
-        
-        /*
-        let movieID = movie["id"]
-        let videoes = "https://api.themoviedb.org/3/movie/\(movieID)/videos?api_key=93e318361fd7d5007c54fbedb1dc26ee&language=en-US"
-        
-        playerView.load(withVideoId: "")*/
-        
     }
 }
