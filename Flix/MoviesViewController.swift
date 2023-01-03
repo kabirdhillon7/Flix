b4ca2d30e093889d99e04398be755909a148c49c
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-01-02 18:28:16 -0800

    Implemented APICall via APICaller Class
    Moved url request into a function, and called it in MoviesVC

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -21,36 +21,29 @@ class MoviesViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-            // This will run when the network request returns
-            if let error = error {
-                print(error.localizedDescription)
+        let apiCaller = APICaller()
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=")!
+        apiCaller.getMovies(toURL: url) { (data, error) in
+            if let error = error  {
+                print("Error getting movies: \(error.localizedDescription)")
+                return
             } else if let data = data {
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                
-                self.movies = dataDictionary["results"] as! [[String:Any]]
-                
+                if data["results"] == nil {
+                    print("Error: No results key found in data")
+                    return
+                }
+                self.movies = data["results"] as! [[String:Any]]
                 self.tableView.reloadData()
-                
-                print(dataDictionary)
-                
-                // TODO: Get the array of movies
-                // TODO: Store the movies in a property to use elsewhere
-                // TODO: Reload your table view data
-                
             }
         }
-        task.resume()
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         print("loading")
         
         // Find the selected movie
-        let cell  = sender as! UITableViewCell
+        let cell = sender as! UITableViewCell
         let indexPath = tableView.indexPath(for: cell)!
         
         let movie = movies[indexPath.row]
--- Flix/Resources/APICaller.swift
+++ Flix/Resources/APICaller.swift
@@ -12,6 +12,30 @@ class APICaller {
     let apiKey: String = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
     
     // Fn to get movies
+    func getMovies(toURL url: URL, completion: @escaping ([String: Any]?, Error?) -> Void) {
+        let requestUrl = URL(string: url.absoluteString + apiKey)!
+        let request = URLRequest(url: requestUrl, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                                
+                completion(dataDictionary, nil)
+                
+                print(dataDictionary)
+                
+                // TODO: Get the array of movies
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
+                
+            }
+        }
+        task.resume()
+    }
+
     
     // Fn to get trailer
 }
