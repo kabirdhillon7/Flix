fb1227b13bcbc759d0b1bc875135082c19219d2a
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-01-06 15:24:43 -0800

    Unit Test
    Test for APICaller getMovies fn

--- FlixTests/FlixTests.swift
+++ FlixTests/FlixTests.swift
@@ -10,6 +10,32 @@ import XCTest
 
 class FlixTests: XCTestCase {
 
+    func testGetMovieData () {
+        /*
+         let api = APICaller.shared
+             let expectation = self.expectation(description: "API response received")
+
+             api.getMovies(toURL: "https://api.themoviedb.org/3/movie/now_playing?api_key=", completion: { (response, error) in
+                 XCTAssertNotNil(response)
+                 XCTAssertNil(error)
+                 expectation.fulfill()
+             })
+
+             waitForExpectations(timeout: 5.0, handler: nil)
+         */
+        let api = APICaller.shared
+        let expectation = self.expectation(description: "API response recieved")
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=")
+        
+        api.getMovies(toURL: url!,
+                      completion: { (response, error) in
+            XCTAssertNotNil(response)
+            XCTAssertNil(error)
+            expectation.fulfill()
+        })
+        
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
