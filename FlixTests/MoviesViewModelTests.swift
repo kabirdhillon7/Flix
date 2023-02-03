03d179b308f19c73fea9a5bee8ca2108686a3b77
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-02-02 18:40:39 -0800

    Setting up MoviesVM Tests

--- FlixTests/MoviesViewModelTests.swift
+++ FlixTests/MoviesViewModelTests.swift
@@ -1,15 +1,14 @@
 //
-//  FlixTests.swift
+//  MoviesViewModelTests.swift
 //  FlixTests
 //
-//  Created by Kabir Dhillon on 11/17/21.
+//  Created by Kabir Dhillon on 2/2/23.
 //
 
 import XCTest
-@testable import Flix
 
-class FlixTests: XCTestCase {
- 
+final class MoviesViewModelTests: XCTestCase {
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -21,6 +20,9 @@ class FlixTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
 
     func testPerformanceExample() throws {
